6a7a7c7902c35d3be4f9a0a1586e7f61919f5ef9
zooeydotmango <newzoe@qq.com>
2019-10-27 02:59:12 +0800

    还原

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "/HeartMusic/"
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
     module: {
