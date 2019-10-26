9d3657e742279a2e7e58a63e9225251baabad369
zooeydotmango <newzoe@qq.com>
2019-10-27 02:36:24 +0800

    修改样式

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/HeartMusic/dist"
+        publicPath: "/HeartMusic/"
     },
     devtool: 'inline-source-map',
     module: {
